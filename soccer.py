a0cb4aa63a2ef11bd3c636ce82281447913d5054
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2025-04-22 12:10:19 +0900

    Create soccer.py

---
+++ soccer.py
@@ -0,0 +1,11 @@
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+    b=p.chromium.launch(headless=True)
+    pa=b.new_page()
+    pa.goto('https://www.kleague.com/record/team.do')
+    t1=pa.locator('table#t1>tbody>tr').first.locator('td').nth(3).text_content()
+    m=pa.locator('table#t1>tbody>tr').nth(1).locator('td').nth(2).text_content()
+    s=pa.locator('table#t1>tbody>tr').nth(1).locator('td').nth(3).text_content()
+    t2=int(m)*int(s)
+    v=int(int(t1)/t2*100)
+    print(f'{v}%')
