6e729b922251142b2e6e5aed44ea5bf02f46f7b6
jingoochoi <140691548+jingoochoi@users.noreply.github.com>
2025-04-27 10:20:26 +0900

    Update macro.py

--- macro.py
+++ macro.py
@@ -1,13 +1,18 @@
 from playwright.sync_api import sync_playwright
+import random
 p = sync_playwright().start()
 browser = p.chromium.launch(headless=False)
 page = browser.new_page()
-
-page.goto('https://chatgpt.com/')
-page.wait_for_timeout(2000)
-page.locator('p.placeholder').fill('오늘의 업계 트렌드')
+delay = random.randint(80, 150)
+page.goto('https://www.naver.com/')
 page.wait_for_timeout(1000)
-page.locator('button#composer-submit-button').click()
+page.locator('a',has_text='스포츠').click()
+page.wait_for_timeout(1000)
+page.locator('a',has_text='스포츠홈').click()
+page.wait_for_timeout(1000)
+page.locator('a',has_text='해외축구').click()
+page.wait_for_timeout(1000)
+page.locator('a',has_text='이강인').click()
 input("종료하려면 Enter 키를 누르세요.")
 browser.close()
 p.stop()
